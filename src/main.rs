6a153acce944c42006997d19001bea6a639084fb
ksakiyama <k.sakiyama1@gmail.com>
2018-04-24 18:36:55 +0900

    second commit

--- src/main.rs
+++ src/main.rs
@@ -1,48 +1,54 @@
 #[macro_use]
 extern crate serde_derive;
+extern crate config;
 extern crate oauth_client as oauth;
 extern crate serde_json;
 
 use oauth::Token;
+use serde_json::Value;
+
 use std::collections::HashMap;
-use serde_json::{Value, Error};
+use std::env;
 
 fn run() {
+    let mut cfg_path = env::home_dir().unwrap();
+    cfg_path.push(".twclirc.yml");
+    cfg_path.as_path();
+
+    let mut settings = config::Config::default();
+    settings
+        .merge(config::File::from(cfg_path.as_path()))
+        .unwrap();
+    let cfg = settings.try_into::<HashMap<String, String>>().unwrap();
+
     let home_timeline: &'static str = "https://api.twitter.com/1.1/statuses/home_timeline.json";
-    let consumer_key = "abc";
-    let consumer_secret = "abc";
-    let access_token = "abv";
-    let access_token_secret = "abv";
 
-    let consumer = Token::new(consumer_key, consumer_secret);
-    let access = Token::new(access_token, access_token_secret);
+    let consumer_key = &cfg["consumer_key"];
+    let consumer_secret = &cfg["consumer_secret"];
+    let access_token = &cfg["access_token"];
+    let access_token_secret = &cfg["access_token_secret"];
 
-    let bytes = oauth::get(home_timeline, &consumer, Some(&access), None).unwrap();
+    let consumer = Token::new(consumer_key.as_str(), consumer_secret.as_str());
+    let access = Token::new(access_token.as_str(), access_token_secret.as_str());
+
+    let mut params = HashMap::new();
+    params.insert("count".into(), "5".into());
+
+    let bytes = oauth::get(home_timeline, &consumer, Some(&access), Some(&params)).unwrap();
     let last_tweets_json = String::from_utf8(bytes).unwrap();
-    let ts : Vec<Value> = serde_json::from_str(&last_tweets_json).unwrap();
+    let tweets: Vec<Value> = serde_json::from_str(&last_tweets_json).unwrap();
 
-    if ts.is_empty() {
+    if tweets.is_empty() {
         println!("No tweet in your timeline...");
     } else {
-        for t in ts {
+        for t in tweets {
             println!("{}::{}", t["user"]["name"], t["text"]);
-            // println!("{}", t["text"]);
             println!("{}", t["created_at"]);
             println!("---------------------------------------------------");
         }
     }
 }
 
-#[derive(Clone, Debug, Serialize, Deserialize)]
-pub struct Tweet {
-    pub created_at: String,
-    pub text: String,
-    // pub user: String,
-    // pub name: String
-    pub user: HashMap<String, String>
-}
-
 fn main() {
     run();
-    println!("Hello, world!");
 }
