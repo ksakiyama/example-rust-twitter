db0b515c9fbd81e5e01f074aaf07fa098a366c0e
ksakiyama <k.sakiyama1@gmail.com>
2018-04-24 21:42:53 +0900

    add struct Twicli

--- src/main.rs
+++ src/main.rs
@@ -10,77 +10,81 @@ use clap::{App, Arg, SubCommand};
 use std::collections::HashMap;
 use std::env;
 
-fn run() {
-    let mut cfg_path = env::home_dir().unwrap();
-    cfg_path.push(".twclirc.yml");
-    cfg_path.as_path();
+static STATUSES_UPDATE: &str = "https://api.twitter.com/1.1/statuses/update.json";
+static HOME_TIMELINE: &str = "https://api.twitter.com/1.1/statuses/home_timeline.json";
 
-    let mut settings = config::Config::default();
-    settings
-        .merge(config::File::from(cfg_path.as_path()))
-        .unwrap();
-    let cfg = settings.try_into::<HashMap<String, String>>().unwrap();
-
-    let home_timeline: &'static str = "https://api.twitter.com/1.1/statuses/home_timeline.json";
-
-    let consumer_key = &cfg["consumer_key"];
-    let consumer_secret = &cfg["consumer_secret"];
-    let access_token = &cfg["access_token"];
-    let access_token_secret = &cfg["access_token_secret"];
-
-    let consumer = Token::new(consumer_key.as_str(), consumer_secret.as_str());
-    let access = Token::new(access_token.as_str(), access_token_secret.as_str());
-
-    let mut params = HashMap::new();
-    params.insert("count".into(), "5".into());
-
-    let bytes = oauth::get(home_timeline, &consumer, Some(&access), Some(&params)).unwrap();
-    let last_tweets_json = String::from_utf8(bytes).unwrap();
-    let tweets: Vec<Value> = serde_json::from_str(&last_tweets_json).unwrap();
-
-    if tweets.is_empty() {
-        println!("No tweet in your timeline...");
-    } else {
-        for t in tweets {
-            println!("{}::{}", t["user"]["name"], t["text"]);
-            println!("{}", t["created_at"]);
-            println!("---------------------------------------------------");
-        }
-    }
+#[derive(Debug)]
+struct Twicli {
+    consumer_key: String,
+    consumer_secret: String,
+    access_token: String,
+    access_token_secret: String,
 }
 
-fn run_tweet(status: &str) {
-    let mut cfg_path = env::home_dir().unwrap();
-    cfg_path.push(".twclirc.yml");
-    cfg_path.as_path();
+impl Twicli {
+    pub fn new() -> Twicli {
+        let mut cfg_path = env::home_dir().unwrap();
+        cfg_path.push(".twclirc.yml");
+        cfg_path.as_path();
 
-    let mut settings = config::Config::default();
-    settings
-        .merge(config::File::from(cfg_path.as_path()))
-        .unwrap();
-    let cfg = settings.try_into::<HashMap<String, String>>().unwrap();
+        let mut settings = config::Config::default();
+        settings
+            .merge(config::File::from(cfg_path.as_path()))
+            .unwrap();
+        let cfg = settings.try_into::<HashMap<String, String>>().unwrap();
 
-    let statues_update: &'static str = "https://api.twitter.com/1.1/statuses/update.json";
-
-    let consumer_key = &cfg["consumer_key"];
-    let consumer_secret = &cfg["consumer_secret"];
-    let access_token = &cfg["access_token"];
-    let access_token_secret = &cfg["access_token_secret"];
-
-    let consumer = Token::new(consumer_key.as_str(), consumer_secret.as_str());
-    let access = Token::new(access_token.as_str(), access_token_secret.as_str());
-
-    let mut params = HashMap::new();
-    params.insert("status".into(), status.into());
-
-    match oauth::post(statues_update, &consumer, Some(&access), Some(&params)) {
-        Ok(_) => {
-            // let resp = String::from_utf8(bytes).unwrap();
-            // println!("{:?}", resp);
-            println!("Success.");
+        Twicli {
+            consumer_key: cfg["consumer_key"].clone(),
+            consumer_secret: cfg["consumer_secret"].clone(),
+            access_token: cfg["access_token"].clone(),
+            access_token_secret: cfg["access_token_secret"].clone(),
         }
-        Err(err) => {
-            println!("{}", err.to_string());
+    }
+
+    pub fn timeline(self, count: &str) {
+        let consumer = Token::new(self.consumer_key.as_str(), self.consumer_secret.as_str());
+        let access = Token::new(
+            self.access_token.as_str(),
+            self.access_token_secret.as_str(),
+        );
+
+        let mut params = HashMap::new();
+        params.insert("count".into(), count.into());
+
+        let bytes = oauth::get(HOME_TIMELINE, &consumer, Some(&access), Some(&params)).unwrap();
+        let last_tweets_json = String::from_utf8(bytes).unwrap();
+        let tweets: Vec<Value> = serde_json::from_str(&last_tweets_json).unwrap();
+
+        if tweets.is_empty() {
+            println!("No tweet in your timeline...");
+        } else {
+            for t in tweets {
+                println!("{}::{}", t["user"]["name"], t["text"]);
+                println!("{}", t["created_at"]);
+                println!("---------------------------------------------------");
+            }
+        }
+    }
+
+    pub fn tweet(self, status: &str) {
+        let consumer = Token::new(self.consumer_key.as_str(), self.consumer_secret.as_str());
+        let access = Token::new(
+            self.access_token.as_str(),
+            self.access_token_secret.as_str(),
+        );
+
+        let mut params = HashMap::new();
+        params.insert("status".into(), status.into());
+
+        match oauth::post(STATUSES_UPDATE, &consumer, Some(&access), Some(&params)) {
+            Ok(_) => {
+                // let resp = String::from_utf8(bytes).unwrap();
+                // println!("{:?}", resp);
+                println!("Success.");
+            }
+            Err(err) => {
+                println!("{}", err.to_string());
+            }
         }
     }
 }
@@ -100,16 +104,35 @@ fn main() {
                 .about("Tweet your status.")
                 .arg(Arg::with_name("status").index(1).required(true)),
         )
+        .subcommand(
+            SubCommand::with_name("tl")
+                .about("Show home timeline.")
+                .arg(Arg::with_name("count").index(1)),
+        )
+        .subcommand(
+            SubCommand::with_name("tw")
+                .about("Tweet your status.")
+                .arg(Arg::with_name("status").index(1).required(true)),
+        )
         .usage("twcli timeline(tl) &{count} / twcli tweet(tw) ${status}")
         .get_matches();
+
+    let twitter = Twicli::new();
 
     match app_m.subcommand_name() {
         Some("timeline") => {
             if let Some(sub_m) = app_m.subcommand_matches("timeline") {
                 match sub_m.value_of("count") {
-                    // set count
-                    Some(c) => println!("{}", c),
-                    None => run(),
+                    Some(c) => twitter.timeline(&c),
+                    None => twitter.timeline("20"),
+                }
+            }
+        }
+        Some("tl") => {
+            if let Some(sub_m) = app_m.subcommand_matches("tl") {
+                match sub_m.value_of("count") {
+                    Some(c) => twitter.timeline(&c),
+                    None => twitter.timeline("20"),
                 }
             }
         }
@@ -117,8 +140,19 @@ fn main() {
             if let Some(sub_m) = app_m.subcommand_matches("tweet") {
                 match sub_m.value_of("status") {
                     Some(s) => {
-                        run_tweet(s);
-                        // println!("{}", s);
+                        twitter.tweet(&s);
+                    }
+                    None => {
+                        println!("Need status.");
+                    }
+                }
+            }
+        }
+        Some("tw") => {
+            if let Some(sub_m) = app_m.subcommand_matches("tw") {
+                match sub_m.value_of("status") {
+                    Some(s) => {
+                        twitter.tweet(&s);
                     }
                     None => {
                         println!("Need status.");
